3e0500abc5776af642319f54305ec1882c017194
gberendsen <georg.berendsen@online.ee>
2016-06-09 18:46:40 +0300

    database set up w. conn/disconn methods

--- src/serpent/model/Database.java
+++ src/serpent/model/Database.java
@@ -1,7 +1,46 @@
 package serpent.model;
 
+import java.sql.*;
+
 /**
  * Created by io on 9.06.16.
  */
 public class Database {
+
+    private Connection connection = null;
+    private Statement statement = null;
+    private String sql = null;
+    private ResultSet resultSet = null;
+
+
+    public Database(){
+        connect();
+        disconnect();
+    }
+
+    private void connect(){
+        try{
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:test.db");
+        }
+        catch (Exception e){
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+            //TODO: log error
+        }
+        //TODO: log success
+    }
+
+    private void disconnect(){
+        try{
+            connection.close();
+        }
+        catch(Exception e){
+            System.out.println(e.getClass().getName() + ": " + e.getMessage());
+            //TODO: log error
+        }
+        //TODO: log success
+    }
+
+
+
 }
